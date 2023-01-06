f97f742097b1c0bf6e577a3e7f0e03bbdecc5e7a
StarProxima <maks.prozorov1@yandex.ru>
2023-01-06 06:18:16 +0300

    More AnsiPen colors

--- lib/src/support/ansi_pen.dart
+++ lib/src/support/ansi_pen.dart
@@ -10,10 +10,27 @@ class AnsiPen {
   const AnsiPen.orange() : color = 208;
   const AnsiPen.purple() : color = 199;
   const AnsiPen.gold() : color = 220;
-  const AnsiPen.yelow() : color = 226;
   const AnsiPen.blue() : color = 39;
   const AnsiPen.navyBlue() : color = 17;
   const AnsiPen.green() : color = 40;
+  const AnsiPen.darkRed() : color = 88;
+  const AnsiPen.darkGreen() : color = 28;
+  const AnsiPen.darkYellow() : color = 136;
+  const AnsiPen.darkBlue() : color = 21;
+  const AnsiPen.darkPurple() : color = 90;
+  const AnsiPen.lightPurple() : color = 213;
+  const AnsiPen.lightGreen() : color = 47;
+  const AnsiPen.lightYellow() : color = 228;
+  const AnsiPen.lightBlue() : color = 45;
+  const AnsiPen.gray() : color = 243;
+  const AnsiPen.darkGray() : color = 236;
+  const AnsiPen.lightGray() : color = 250;
+  const AnsiPen.beige() : color = 230;
+  const AnsiPen.brown() : color = 130;
+  const AnsiPen.pink() : color = 218;
+  const AnsiPen.lightRed() : color = 201;
+  const AnsiPen.yellow() : color = 226;
+  const AnsiPen.cyan() : color = 51;
 
   final int? color;
 
